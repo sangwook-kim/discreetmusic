d11fb9ed9d1719dc419a623e5af903b0cfe80929
sangwook kim <mr.galien@gmail.com>
2019-01-03 22:32:38 +0900

    first oscillator, filter, envelopfilter

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-const Tone = require('Tone');
+document.addEventListener('click', () => {
+  const Tone = window.Tone;
 
-const synth = new Tone.MonoSynth({
-  oscillator: {type: 'sawtooth'},
+  const synth = new Tone.MonoSynth({
+    oscillator: {type: 'sawtooth'},
+    envelope: {
+      attack: 0.1,
+      release: 4,
+      releaseCurve: 'linear',
+    },
+    filterEnvelope: {
+      baseFrequency: 200,
+      octaves: 2,
+      attack: 0,
+      decay: 0,
+      release: 1000,
+    },
+  });
+  synth.toMaster();
+  synth.triggerAttackRelease('C4', 1);
 });
-synth.toMaster();
-synth.triggerAttackRelease('C4', 1);
