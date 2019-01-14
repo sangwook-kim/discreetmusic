aab5a96189e1d483d451776cd729e4091be5e265
sangwook-kim <sangwook.kim@navercorp.com>
2019-01-14 19:04:10 +0900

    finish discrete music

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 const { Tone } = window;
-Tone.Transport.bpm.value = 240;
+// Tone.Transport.bpm.value = 120;
+Tone.Transport.bpm.value = 244;
+
+const noUiSlider = require('nouislider');
+
+const EQUALIZER_CENTER_FREQUENCIES = [
+  100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250,
+  1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000,
+];
 
 function makeSynth() {
   const envelope = {
@@ -33,11 +41,53 @@ function makeSynth() {
   });
 }
 
+function initEqualizerUI(container, equalizer) {
+  equalizer.forEach(eq => {
+    const frq = eq.frequency.value;
+
+    const wrapper = document.createElement('div');
+    const slider : any = document.createElement('div');
+    const label = document.createElement('label');
+
+    wrapper.classList.add('slider-wrapper');
+    slider.classList.add('slider');
+    label.textContent = frq < 1000 ? frq : `${frq / 1000}K`;
+
+    noUiSlider.create(slider, {
+      start: 0,
+      range: {min: -12, max: 12},
+      step: 0.1,
+      orientation: 'vertical',
+      direction: 'rtl',
+    });
+
+    slider.noUiSlider.on('update', ([value]) => {
+      console.log(value);
+      eq.gain.value = value;
+    });
+
+    wrapper.appendChild(slider);
+    wrapper.appendChild(label);
+    container.appendChild(wrapper);
+  });
+}
+
 const leftSynth = makeSynth();
 const rightSynth = makeSynth();
 
 const leftPanner = new Tone.Panner(-0.5);
 const rightPanner = new Tone.Panner(0.5);
+
+const equalizer = EQUALIZER_CENTER_FREQUENCIES.map(frequency => {
+  const filter = Tone.context.createBiquadFilter();
+  filter.type = 'peaking';
+  filter.frequency.value = frequency;
+  filter.Q.value = 4.31;
+  filter.gain.value = 0;
+
+  return filter;
+});
+
 const echo = new Tone.FeedbackDelay('16n', 0.2).toMaster();
 const delay = Tone.context.createDelay(6.0); // Borrow the AudioContext From Tone.js
 const delayFade = Tone.context.createGain();
@@ -48,15 +98,39 @@ delayFade.gain.value = 0.75;
 leftSynth.connect(leftPanner);
 rightSynth.connect(rightPanner);
 
+// Serial
+leftPanner.connect(equalizer[0]);
+rightPanner.connect(equalizer[0]);
+
+equalizer.forEach((eq, i) => {
+  if (i < equalizer.length - 1) {
+    equalizer[i].connect(equalizer[i + 1]);
+  } else {
+    equalizer[i].connect(echo);
+  }
+});
+
+/*
+// Parallel
+equalizer.forEach((eq) => {
+  leftPanner.connect(eq);
+  rightPanner.connect(eq);
+
+  eq.connect(echo);
+});
+*/
+
+/*
 leftPanner.connect(echo);
 rightPanner.connect(echo);
+*/
 
 echo.connect(delay);
 delay.connect(Tone.context.destination);
 delay.connect(delayFade);
 delayFade.connect(delay);
 
-document.addEventListener('click', () => {
+document.querySelector('.start').addEventListener('click', (e) => {
   new Tone.Loop((time) => {
     leftSynth.triggerAttackRelease('C5', '1:2', time);
     leftSynth.setNote('D5', '+0:2');
@@ -67,6 +141,7 @@ document.addEventListener('click', () => {
     leftSynth.setNote('A5', '+19:3:0');
     leftSynth.setNote('G5', '+19:4:2');
   }, '34m').start();
+
   new Tone.Loop(() => {
     rightSynth.triggerAttackRelease('D4', '1:2', '+5:0');
     rightSynth.setNote('E4', '+6:0');
@@ -74,5 +149,13 @@ document.addEventListener('click', () => {
     rightSynth.setNote('G3', '+12:0:2');
     rightSynth.triggerAttackRelease('G4', '0:2', '+23:2');
   }, '37m').start();
+  console.log(e);
+
   Tone.Transport.start();
+  initEqualizerUI(document.querySelector('.eq'), equalizer);
+});
+
+document.querySelector('.stop').addEventListener('click', (e) => {
+  console.log(e);
+  Tone.context.close();
 });
